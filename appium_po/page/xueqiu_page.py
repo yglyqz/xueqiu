faabdafb4d2ae7c00de9c5976a495a0a3f6475d9
liyang <746597961@qq.com>
2019-09-22 16:47:17 +0800

    等待某个元素出现(升级弹窗取消icon)

--- appium_po/page/xueqiu_page.py
+++ appium_po/page/xueqiu_page.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 from appium_po.page.profile_page import ProfilePage
 from appium_po.page.search_page import SearchPage
 
@@ -17,7 +21,18 @@ class XueqiuPage:
         from appium import webdriver
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", capabilities)
         self.driver.implicitly_wait(10)
-        # cls.driver.find_element_by_id("image_cancel").click()
+        # self.driver.find_element_by_id("image_cancel").click()
+        #等待某个元素出现
+        WebDriverWait(self.driver, 60).until(
+            expected_conditions.invisibility_of_element_located((By.ID, "image_cancel"))
+        )
+
+        def click_cancel():
+            if self.driver.find_element_by_id("image_cancel").is_displayed():
+                self.driver.find_element_by_id("image_cancel").click()
+            return len(self.driver.find_element_by_id("image_cancel").is_displayed()) >= 1
+        WebDriverWait(self.driver, 60).until(click_cancel)
+
         self.driver.find_element_by_id("user_profile_icon")
 
     def goto_search(self):
